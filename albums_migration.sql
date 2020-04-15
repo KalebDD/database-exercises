92c1c96b407fb6862186706875dbdcfc7271ff3f
Kaleb Dworsky <dworsky.kaleb@gmail.com>
2020-04-15 10:44:59 -0500

    completed exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(100) DEFAULT 'NONE',
+    release_date INT UNSIGNED NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50) DEFAULT 'NONE',
+    PRIMARY KEY (id)
+);
